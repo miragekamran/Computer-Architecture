1c9f5a23761946c7da72a5d83c12d02f849aed7b
mirage <mirage.kamran.pkt@gmail.com>
2020-11-19 09:31:40 -0800

    mvp day 1, 2, 3

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,15 @@
 import sys
 from cpu import *
 
-cpu = CPU()
+def main(argv):
+    """Main."""
+    if len(argv) != 2:
+        print(f"usage: {argv[0]} filename", file=sys.stderr)
+        return 1
 
-cpu.load()
-cpu.run()
+    cpu = CPU()
+    cpu.load(argv[1])
+    cpu.run()
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
